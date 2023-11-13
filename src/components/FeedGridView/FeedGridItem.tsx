446d92c29d784c486d9eb9d66b46dcff9d29cfa5
nithianandan90 <n.maniam1990@gmail.com>
2023-11-13 23:19:10 +0800

    version patch

--- src/components/FeedGridView/FeedGridItem.tsx
+++ src/components/FeedGridView/FeedGridItem.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import colors from '../../theme/colors';
 import {Post} from '../../API';
+import {S3Image} from 'aws-amplify-react-native';
 
 const FeedGridItem = ({post}: {post: Post}) => {
   return (
@@ -15,7 +16,7 @@ const FeedGridItem = ({post}: {post: Post}) => {
         maxWidth: '33.33%',
         padding: 1,
       }}>
-      <Image source={{uri: post.image || post.images?.[0]}} style={{flex: 1}} />
+      <S3Image imgKey={post.image || post.images?.[0]} style={{flex: 1}} />
 
       {post.images && (
         <MaterialIcons
